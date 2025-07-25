66ba7d86c21b43e12fb59ada86b6ea3f98504549
ipcarepi <hackedipod@naver.com>
2025-07-25 14:47:09 +0900

    first commit

---
+++ src/test/java/com/sc/secondHandMarket/SecondHandMarketApplicationTests.java
@@ -0,0 +1,13 @@
+package com.sc.secondHandMarket;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SecondHandMarketApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
